cc24ec230135f1ff578c2bc09550fa0e0047496f
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-02-22 21:29:35 -0600

    refactored and tested challenges

--- solarSystem.js
+++ solarSystem.js
@@ -1,23 +1,18 @@
-const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
-
-const outEl = document.querySelector("#planets")
-outEl.innerHTML = "<h1>Planets in Our Solar System</h1>"
+const planets = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune']
+/*
+    Use the forEach method to add the name of each planet
+    to a section element in your HTML with an id of 'planets'.
+    Use string templates to construct the DOM elements.
+*/
+const outEl = document.querySelector('#planets')
+outEl.innerHTML = '<h1>Planets in Our Solar System</h1>'
 
 planets.forEach(planet => {
   outEl.innerHTML += `
     <h2>${planet.elemnt}</h2>
   `
-  outEl.innerHTML += "<hr/>"
+  outEl.innerHTML += '<hr/>'
 });
-/*
-    Use the forEach method to add the name of each planet
-    to a section element in your HTML with an id of "planets".
-    Use string templates to construct the DOM elements.
-*/
-const planetEl = document.getElementById("planets")
-function capitalizeFirstLetter(planets) {
-  return planets[0].toUpperCase() + planets.slice(1);
-}
 /*
     Use the map method to create a new array where the
     first letter of each planet is capitalized. Use the
@@ -25,8 +20,16 @@ function capitalizeFirstLetter(planets) {
 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 */
-const findPlanet = planets.find(planet => planet.includes('e'));
-
+const planetEl = document.getElementById('planets')
+const bigPlanet = planets.map(planet => {
+  return planet.charAt(0).toUpperCase() + planet.slice(1);
+});
+bigPlanet.forEach((planet) => {
+  planetEl.innerHTML += `
+  <h2>${planet}</h2>
+   `;
+  planetEl.innerHTML += '<hr/>';
+});
 /*
     Use the filter method to create a new array that
     contains planets with the letter 'e'. Use the `includes()`
@@ -34,3 +37,20 @@ const findPlanet = planets.find(planet => planet.includes('e'));
 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 */
+const findPlanet = planets.filter(planet => {
+  let containsE = false;
+
+  if (planet.includes('e')) {
+    containsE = true;
+  };
+
+  return containsE;
+});
+
+
+ePlanets.forEach((planet) => {
+  planetEl.innerHTML += `
+  <h2>${planet}</h2>
+`;
+  planetEl.innerHTML += '<hr/>';
+});
